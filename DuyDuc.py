71de4246bd96656f13ed0ab2750c5dbbeb684f16
Lê Thị Thuỳ Trang <thuytrangneee@MacBook-Pro-cua-Le.local>
2025-11-04 02:41:14 +0700

    update

--- DuyDuc.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("datasetprj.xlsx")
-df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
-df_2012 = df[df['Date'].dt.year == 2012]
-df_2012.to_excel("datasetprj_2012.xlsx", index=False)
-print(df.head(5))
